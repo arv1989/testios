46edd81a1e88aed410a3259c7e35090e31012ed6
Ameen Rashad <ameen@mocioun.com>
2016-06-10 14:53:43 +0530

    Initial Commit

---
+++ dailybibleverses/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  dailybibleverses
+//
+//  Created by Ameen Rashad on 6/10/16.
+//  Copyright (c) 2016 Zamuari. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ dailybibleverses/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  dailybibleverses
+//
+//  Created by Ameen Rashad on 6/10/16.
+//  Copyright (c) 2016 Zamuari. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
